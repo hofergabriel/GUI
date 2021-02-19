74477982953494ec6a7d285c2f8317b983d50fa0
Gabriel Hofer <leirbagabriel468@gmail.com>
2021-02-19 16:09:35 -0700

    Update ApartmentComplex.java

--- ApartmentComplex.java
+++ ApartmentComplex.java
@@ -5,7 +5,10 @@ import javafx.scene.layout.VBox;
 import java.util.ArrayList;
 
 public class ApartmentComplex {
-    private int currMonth, funds;
+    private int FUNDS=100000;
+    private int MAINTENANCE=0;
+    private int COLLECTRENT=0;
+    private int FILLED=0;
     private ArrayList<ArrayList<Apartment>> apartments = new ArrayList<ArrayList<Apartment>>();
 
     public void increaseRent(int x){
@@ -37,10 +40,43 @@ public class ApartmentComplex {
         }
     }
 
+    public void updateCollectRent() {
+        COLLECTRENT = 0;
+        for (int i = 0; i < apartments.size(); i++)
+            for (int j = 0; j < apartments.get(i).size(); j++)
+                COLLECTRENT+=apartments.get(i).get(j).getRent();
+    }
+
+    public void updateMaintenance(){
+        MAINTENANCE=0;
+        for(int i=0;i<apartments.size();i++)
+            for (int j = 0; j < apartments.get(i).size(); j++)
+                MAINTENANCE+=apartments.get(i).get(j).getMaintenance();
+    }
+
+    public int getCOLLECTRENT(){ return COLLECTRENT; }
+    public int getMAINTENANCE(){ return MAINTENANCE; }
+
+    public void updateFunds(ArrayList<VBox> buildings){
+        this.updateCollectRent();
+        this.updateMaintenance();
+        for(int i=0;i<apartments.size();i++)
+            for(int j=0;j<apartments.get(i).size();j++)
+                FUNDS=FUNDS+this.getCOLLECTRENT()-this.getMAINTENANCE();
+    }
+
+    public void updateFILLED(){ FILLED+=1; }
+    public int getFILLED(){ return FILLED; }
+
+    public void newEmptyUpdateFunds(){ FUNDS-=20000; }
     public ApartmentComplex(){ }
     public void clearApartmentComplex(){ apartments.clear(); }
     public void addBuilding(){ apartments.add(new ArrayList<Apartment>()); }
     public void addApartment(int b, Apartment a){ apartments.get(b).add(a); }
     public void setApartment(int bui, int flo, Apartment a){ apartments.get(bui).set(flo,a); }
     public int getBuildingHeight(int bui){ return apartments.get(bui).size(); }
+    public int getFUNDS(){ return FUNDS; }
 }
+
+
+

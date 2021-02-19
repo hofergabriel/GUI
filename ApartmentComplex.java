2aa127034e3d8adcb9f14907bf43f832703abc32
Gabriel Hofer <leirbagabriel468@gmail.com>
2021-02-18 22:24:17 -0700

    Update ApartmentComplex.java

--- ApartmentComplex.java
+++ ApartmentComplex.java
@@ -5,12 +5,40 @@ public class ApartmentComplex {
     private int currMonth, funds;
     private ArrayList<ArrayList<Apartment>> apartments = new ArrayList<ArrayList<Apartment>>();
 
-    public void increaseRent(int amt){ }
-    public void newMonth(){}
-    public ApartmentComplex(){ }
+    public void increaseRent(int x){
+        for(int i=0;i<apartments.size();i++){
+            for(int j=0;j<apartments.get(i).size();j++){
+                apartments.get(i).get(j).increaseRent(x);
+            }
+        }
+    }
+    public void newMonth(){
+        for(int i=0;i<apartments.size();i++){
+            for(int j=0;j<apartments.get(i).size();j++){
+                apartments.get(i).get(j).newMonth();
+            }
+        }
+    }
+    public void showRents(){
+        System.out.println("rows: "+apartments.size());
+        for(int i=0;i<apartments.size();i++){
+            System.out.println("cols: "+apartments.get(i).size());
+            for(int j=0;j<apartments.get(i).size();j++){
+                System.out.println("rent!: "+apartments.get(i).get(j).getRent());
+                if(apartments.get(i).get(j) instanceof Empty)
+                    System.out.println("EMPTY");
+                if(apartments.get(i).get(j) instanceof Basic)
+                    System.out.println("BASIC");
+                if(apartments.get(i).get(j) instanceof Penthouse)
+                    System.out.println("PENTHOUSE");
+            }
+        }
 
+    }
+
+
+    public ApartmentComplex(){ }
     public void clearApartmentComplex(){ apartments.clear(); }
     public void addBuilding(){ apartments.add(new ArrayList<Apartment>()); }
     public void addApartment(int b, Apartment a){ apartments.get(b).add(a); }
-
 }

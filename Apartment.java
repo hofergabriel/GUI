d116992b2b729bea5813053787e10fc77a583b1f
Gabriel Hofer <leirbagabriel468@gmail.com>
2021-02-19 16:09:57 -0700

    Update Apartment.java

--- Apartment.java
+++ Apartment.java
@@ -1,8 +1,12 @@
 package hofer_gabriel;
 
 public class Apartment {
-    private int rent, maintenance, remodel, timeSinceRemodel;
+    private int rent=0, maintenance=200, timeSinceRemodel=0;
     public void increaseRent(int x){ rent+=x; }
     public void newMonth(){ timeSinceRemodel+=1; }
     public int getRent(){ return rent; }
+    public int getDuration(){ return timeSinceRemodel; }
+    public int getMaintenance(){ return maintenance; }
+    public void setRent(int x){ rent=x; }
+    public void setMaintenance(int x){ maintenance=x; }
 }
